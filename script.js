c4d95d1332252debcf023f5cbefd4b33692c8c64
TooManyFoxes <140508350+TooManyFoxes@users.noreply.github.com>
2025-10-25 14:06:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var url_string = window.location.href; 
+var url = new URL(url_string);
+var check = url.searchParams.get("utm_source");
+
+if (check = "chatgpt.com") {
+    window.location.replace("https://toomanyfoxes.org/ai");
+};
+
 console.log("nya");
