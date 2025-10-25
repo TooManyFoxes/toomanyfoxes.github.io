79db22248d45eafaddc5b43495c8d68c41e1c2ca
TooManyFoxes <140508350+TooManyFoxes@users.noreply.github.com>
2025-10-25 14:33:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 var url_string = window.location.href; 
 var url = new URL(url_string);
 var check = url.searchParams.get("utm_source");
-console.log(url_string);
-console.log(url);
-console.log(check);
-
-//pls do not hate on my terrible js T-T
 
 if (check == "chatgpt.com" || check == "triggeraiflag") {
     console.log("Check failed.");
