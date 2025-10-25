1edce408f67d2a35b4d266b7c58452d9a0006932
TooManyFoxes <140508350+TooManyFoxes@users.noreply.github.com>
2025-10-25 14:18:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var url_string = window.location.href; 
 var url = new URL(url_string);
 var check = url.searchParams.get("utm_source");
+console.log(url_string);
+console.log(url);
 console.log(check);
 
-if (check != "chatgpt.com") {
-    window.location.replace("https://toomanyfoxes.org/ai");
+if (check == "chatgpt.com" || check == "triggeraiflag") {
     console.log("Check failed.");
 } else {
     console.log("Passed check.");
 }
-
 console.log("nya");
